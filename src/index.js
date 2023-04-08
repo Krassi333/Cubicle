bbe8190710248da83aef1165d9d42024768aeda4
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-08 15:27:58 +0300

    Render home page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
 
-app.use('/static',express.static('public'));
+app.use('/static', express.static('public'));
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views','./src/views');
 
 app.get('/', (req, res) => {
-    res.send('Hi');
+    res.render('index'); 
 });
 
 app.listen(5000, () => console.log('The server is listening on port 5000...'))
