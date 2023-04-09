2003276b291ec8caeb8629973542fce248b3412e
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-04-09 13:51:07 +0300

    Fix image routes

--- src/routs.js
+++ src/routs.js
@@ -2,7 +2,7 @@ const express = require('express');
 const homeController=require('./controllers/homeController');
 const router=express.Router();
 
-router.get('/', homeController);
+router.get('/', homeController.index);
 
 module.exports=router;
 
